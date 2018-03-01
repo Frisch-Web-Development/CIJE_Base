722d037fd659286c6295c2eea2f447a057315278
Michael Avrukin <avrukin@avrukin-macbookpro.roam.corp.google.com>
2018-03-01 10:43:45 -0500

    setup workspace for project

---
+++ data/resources_list.py
@@ -0,0 +1,7 @@
+from resource import Resource
+
+jewish_resources = [
+  Resource("Feed Me Now", "restaurant", "123 1st Street, New York, NY - 555-5555", 123.123, -123.123, "stub"),
+  Resource("Daven Now", "shul", "234 2nd Street, New York, NY - 444-4444", 44.55, 44.55, "stub 2"),
+  Resource("Buy Now", "store", "345 3rd Street, New York, NY - 666-6666", 23.8, 15, "stub 3")]
+
