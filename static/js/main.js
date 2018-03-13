08212af1cb9dff4b7ffd346edb66a2d8d7b62a9e
doc <luke.czapla@frisch.org>
2018-03-13 16:42:29 -0400

    Mapping API usage updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,18 @@
 
-var map;
+var map;    // the Google map
+var y = 40.739915, x = -73.999459;    // coordinates of where we are located, default Manhattan
 
 function initMap() {
-  let latitude = 40.739915; let longitude = -73.999459;
+  let latitude = y; let longitude = x;
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
-        map.setCenter({lat: latitude, lng: longitude});
+        y = latitude; x = longitude;
+        map.setCenter({lat: y, lng: x});
     });
   }
-  let local = {lat: latitude, lng: longitude};
+  let local = {lat: y, lng: x};
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
     center: local
@@ -22,8 +24,16 @@ function addPlaces(places) {
         console.log(p)
         let local = {lat: parseFloat(p.latitude), lng: parseFloat(p.longitude)};
         let marker = new google.maps.Marker({
-          position: local,
-          map: map
+            position: local,
+            title: p.name,
+            map: map
+        });
+        let infowindow = new google.maps.InfoWindow({
+            content: p.other
+        });
+        marker.setLabel(p.type);
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
         });
     }
 }
@@ -41,4 +51,16 @@ window.onload = function() {
         addPlaces(data);
     });
 
+
+    $('#exampleModal').on('show.bs.modal', function(event) {
+          var button = $(event.relatedTarget) // Button that triggered the modal
+          var recipient = button.data('whatever') // Extract info from data-* attributes
+          // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+          // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+          var modal = $(this)
+          modal.find('.modal-title').text('New message to ' + recipient)
+          modal.find('.modal-body input').val(recipient)
+    })
+
 };
+
